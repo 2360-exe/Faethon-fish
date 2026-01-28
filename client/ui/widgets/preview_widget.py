ca8fef88e865ca176082a9e2a045271e6212bbaf
Faethon-college <23002396@gateshead.ac.uk>
2026-01-28 12:02:58 +0000

    Update preview_widget.py im such a muddy puppy
    skate nation fursonas when
    my c # class suckssss
    dead ass so easy

--- client/ui/widgets/preview_widget.py
+++ client/ui/widgets/preview_widget.py
@@ -33,9 +33,9 @@ class PreviewWidget(QWidget):
         self.h = h
         self.result = result
 
-        img = QImage(raw, w, h, QImage.Format.Format_RGBA8888)
+        bytes_per_line = W * 4
+        img = QImage(raw, w, h, bytes_per_line, QImage.Format.Format_RGBA8888)
         self._qimage = img.copy()
-
         self.update()
 
     def paintEvent(self, event):
@@ -52,7 +52,38 @@ class PreviewWidget(QWidget):
             Qt.KeepAspectRatio,
             Qt.SmoothTransformation,
         )
-        painter.drawImage(0, 0, scaled)
+        #daniel im a busy busy girl - faye
+        offset_x =(self.width() - scaled.width()) // 2
+        offset_y =(self.height()- scaled.height()) // 2
+        painter.drawImage(offset_x, offset_y, scaled)
+
+        if not self.show_overlay or not self.result:
+            return
+        if not self.w <= 0 or self.h <= 0:
+            return
+        # coding python in my c# class like a rebel
+        scale_y = scaled.height() / self.h
+
+        pen_white = QPen(QColor(255, 255, 255), 2)
+        pen_black = QPen(QColor(0, 0, 0), 2)
+        pen_text = QPen(QColor(255, 255, 0))
+
+        if self.result.white_y is not None:
+            wy = int(self.result.white_y * scale_y) + offset_y
+            painter.setPen(pen_white)
+            painter.drawLine(offset_x, wy, offset_x +scaled.width(), wy)
+
+        if self.result.bar_y is not None:
+            by = int(self.result.bar_y * scale_y) + offset_y
+            painter.setPen(pen_black)
+            painter.drawLine(offset_x, by, offset_x + scaled.width() , by)
+
+        if self.result.distance is not None:
+            painter.setPen(pen_text)
+            painter.setFont(QFont("Arial", 12))
+            painter.drawText(offset_x + 10, offset_y + 20, f"dist={self.result.distance:.1f}")
+        
+        """painter.drawImage(0, 0, scaled)
 
         if not self.show_overlay or not self.result:
             return
@@ -82,4 +113,4 @@ class PreviewWidget(QWidget):
         if self.result.distance is not None:
             painter.setPen(pen_text)
             painter.setFont(QFont("Arial", 12))
-            painter.drawText(10, 20, f"dist={self.result.distance:.1f}")
+            painter.drawText(10, 20, f"dist={self.result.distance:.1f}")"""
